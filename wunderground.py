78ce43b7582781b0fc13240ba601a79d9a54f840
Daniel Bergey <bergey@alum.mit.edu>
2012-07-24 14:13:30 -0400

    Rewrite to use numpy.loadtxt

--- wunderground.py
+++ wunderground.py
@@ -1,29 +1,44 @@
+# coding=utf-8
 # originally from http://stackoverflow.com/questions/5061446/api-with-past-weather
 import urllib2
-import csv 
-import datetime
+import numpy as np
+from matplotlib.dates import datestr2num
+import re
+from functional import compose
+
+def br_strip(str):
+    return re.sub(r'<.*$', '', str)
 
 """Gets daily temperatures from Weather Underground"""
 
-def get_temperature_data(airport, date):
+class Weather():
+    """For now, only has columns which make sense as numbers"""
+    date = None # UTC Time
+    T = None # Temp [°F]
+    Dp = None # Dewpoint [°F]
+    RH = None # 0-100
+    P = None # in Hg at sea level?
+    Vis = None # Visibility (Miles?)
+# needs Calm parser    MPH_avg = None # Wind Speed
+# needs - parser    MPH_gust = None # Wind Gust Speed
+    Wind_Angle = None # Degrees
+# needs N/A parser    Rain = None # Inches
+
+def day_weather(airport, date, opener=None):
     url = get_url(airport, date.year, date.month, date.day)
 
-    opener = urllib2.build_opener()
-
-    data = []
+    if opener==None:
+        opener = urllib2.build_opener()
 
     f = opener.open(url)
-    reader = csv.reader(f)
-    for row in reader:
-        if len(row) > 1:
-            # print row[0], row[1]
-            time = datetime.datetime.strptime(row[0],"%I:%M %p")
-            dt = datetime.datetime.combine(date.date(), time.time()) 
-            temp = float(row[1])
-            # print dt, temp
-            data.append({'time':dt, 'temperature':temp})
 
-    return data
-
+    return np.loadtxt(f,
+                      skiprows=2,
+                      delimiter=',',
+                      usecols=[1,2,3,4,5,12,13],
+                      unpack=True,
+                      converters = {13: compose(datestr2num,br_strip)}
+                      )
+    
 def get_url(airport, year, month, day):
     return 'http://www.wunderground.com/history/airport/{}/{:02d}/{:02d}/{:02d}/DailyHistory.html?req_city=NA&req_state=NA&req_statename=NA&format=1'.format(airport, year, month, day)
