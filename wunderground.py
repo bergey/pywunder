93e9ca98a8797e3b5472649d5d2e941f11a20a78
Daniel Bergey <bergey@alum.mit.edu>
2012-10-17 15:30:07 -0400

    NAN parser, loop over interval

--- wunderground.py
+++ wunderground.py
@@ -5,9 +5,20 @@ import numpy as np
 from matplotlib.dates import datestr2num
 import re
 from functional import compose
+from datetime import datetime
 
 def br_strip(str):
     return re.sub(r'<.*$', '', str)
+
+def NA2NaN(str):
+    if str=='N/A':
+        return np.NaN
+    else:
+        return float(str)
+
+class ReversedRange(Exception):
+    def __init__(self):
+        super(ReversedRange, self).__init__("End of range is smaller than beginning")
 
 """Gets daily temperatures from Weather Underground"""
 
@@ -22,7 +33,7 @@ class Weather():
 # needs Calm parser    MPH_avg = None # Wind Speed
 # needs - parser    MPH_gust = None # Wind Gust Speed
     Wind_Angle = None # Degrees
-# needs N/A parser    Rain = None # Inches
+    Rain = None # Inches
 
 def day_weather(airport, date, opener=None):
     url = get_url(airport, date.year, date.month, date.day)
@@ -32,13 +43,32 @@ def day_weather(airport, date, opener=None):
 
     f = opener.open(url)
 
+    print(date.strftime("parsing %Y-%m-%d")
     return np.loadtxt(f,
                       skiprows=2,
                       delimiter=',',
-                      usecols=[1,2,3,4,5,12,13],
+                      usecols=[1,2,3,4,5,9,12,13],
                       unpack=True,
-                      converters = {13: compose(datestr2num,br_strip)}
+                      converters = {13: compose(datestr2num,br_strip),
+                                     3: NA2NaN,
+                                     9: NA2NaN}
                       )
+
+def interval_weather(airport, start, stop):
+    """return a Weather object with all data from start to stop"""
+    if stop < start:
+        raise ReversedRange
+    opener = urllib2.build_opener()
+    day = datetime(1970,1,2) - datetime(1970,1,1)
+    d = start
+    ws = []
+    while d<stop:
+        ws.append(day_weather(airport, d, opener))
+        d += day
+    
+    ret = Weather()
+    ret.T, ret.Dp, ret.RH, ret.P, ret.Vis, ret.Rain, ret.Wind_Angle, ret.date = np.hstack(ws)
+    return ret
     
 def get_url(airport, year, month, day):
     return 'http://www.wunderground.com/history/airport/{}/{:02d}/{:02d}/{:02d}/DailyHistory.html?req_city=NA&req_state=NA&req_statename=NA&format=1'.format(airport, year, month, day)
